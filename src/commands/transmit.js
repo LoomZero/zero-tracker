d3ddf0b8e3afe13490ca1c06e3b75c2109ee1cc5
LoomZero <p.sagitza@loom.de>
2021-11-14 16:45:27 +0100

    add yes and update merge algoryth

--- src/commands/transmit.js
+++ src/commands/transmit.js
@@ -11,8 +11,12 @@ const CLITable = require('../util/CLITable');
  * @property {import('../connector/RedmineConnector')} redmine
  * @property {string} state
  * @property {number} [hours]
+ * @property {Moment.Moment} [when]
  * @property {string} [comment]
- * @property {number[]} [merged]
+ * @property {import('../../types').T_TogglTracking[]} [merged]
+ * @property {string[]} [info]
+ * @property {number} [activity]
+ * @property {any} [customFields]
  */
 
 module.exports = class TransmitCommand extends Command {
@@ -30,6 +34,8 @@ module.exports = class TransmitCommand extends Command {
       .option('--dry', 'Dry run', false)
       .option('--round <round>', 'Round to x min', this.fallback('round', false))
       .option('--merge', 'Merge tickets hours', this.fallback('merge', false))
+      .option('-f, --force <force>', 'Force the unknown issue to a state (Allowed: skip (s), ignore (i))', this.fallback('force', false))
+      .option('-y, --yes', 'Accept the transmit', this.fallback('yes', false))
       .usage('--from "-1 weeks" --to "now"')
       .description('transmit trackings from toggl to redmine');
   }
@@ -39,6 +45,16 @@ module.exports = class TransmitCommand extends Command {
       this.error('Please create a fallback redmine connection.');
       this.error('Use "tracker redmine add" to create a connection.');
       return;
+    }
+
+    if (this.opts.force) {
+      if (['skip', 's', 'ignore', 'i'].includes(this.opts.force)) {
+        if (this.opts.force === 's') this.opts.force = 'skip';
+        if (this.opts.force === 'i') this.opts.force = 'ignore';
+      } else {
+        this.error('Only "skip" or "ignore" are valid values for option --force');
+        return;
+      }
     }
 
     this.toggl = await this.tracker.getToggl();
@@ -68,34 +84,38 @@ module.exports = class TransmitCommand extends Command {
         issue = issueMatch[1];
       }  else {
         if (!this.opts.dry) {
-          console.log('No issue ID found on tracking "' + description + ' [' + tracking.id + ']"');
-          issue = await this.tracker.input('Issue Number (#/s/i/?): ', async (answer) => {
-            if (this.isInt(answer)) {
-              try {
-                console.log('Ticket: ' + (await redmine.getIssue(Number.parseInt(answer))).subject);
-              } catch (e) {
-                const error = JSON.parse(e);
-                if (error.ErrorCode === 404) {
-                  return 'This issue is not known.';
+          if (this.opts.force) {
+            issue = this.opts.force;
+          } else {
+            console.log('No issue ID found on tracking "' + description + ' [' + tracking.id + ']"');
+            issue = await this.tracker.input('Issue Number (#/s/i/?): ', async (answer) => {
+              if (this.isInt(answer)) {
+                try {
+                  console.log('Ticket: ' + (await redmine.getIssue(Number.parseInt(answer))).subject);
+                } catch (e) {
+                  const error = JSON.parse(e);
+                  if (error.ErrorCode === 404) {
+                    return 'This issue is not known.';
+                  }
                 }
+                if (await this.tracker.input('Is this the ticket? (y/n): ') !== 'y') {
+                  return 'abort';
+                } else {
+                  description = '#' + answer + ' - ' + description;
+                  await this.toggl.updateTimeEntry(tracking.id, { description });
+                  console.log('UPDATE: ' + description + ' [' + tracking.id + ']');
+                }
+              } else if (answer === 'help' || answer === '?') {
+                console.log('Help:');
+                console.log('Use "ignore" or "i" to ignore the tracking in further transmit.');
+                console.log('Use "skip" or "s" to ignore the tracking in current transmit.');
+                console.log('Use "help" or "?" to show current help');
+                return '';
+              } else if (!['skip', 'ignore', 's', 'i'].includes(answer)) {
+                return 'Unknown option.'
               }
-              if (await this.tracker.input('Is this the ticket? (y/n): ') !== 'y') {
-                return 'abort';
-              } else {
-                description = '#' + answer + ' - ' + description;
-                await this.toggl.updateTimeEntry(tracking.id, { description });
-                console.log('UPDATE: ' + description + ' [' + tracking.id + ']');
-              }
-            } else if (answer === 'help' || answer === '?') {
-              console.log('Help:');
-              console.log('Use "ignore" or "i" to ignore the tracking in further transmit.');
-              console.log('Use "skip" or "s" to ignore the tracking in current transmit.');
-              console.log('Use "help" or "?" to show current help');
-              return '';
-            } else if (!['skip', 'ignore', 's', 'i'].includes(answer)) {
-              return 'Unknown option.'
-            }
-          });
+            });
+          }
           if (issue === 'skip' || issue === 's') {
             transmitting.push({
               tracking, description, redmine, state: 'skip'
@@ -103,7 +123,6 @@ module.exports = class TransmitCommand extends Command {
             continue;
           }
           if (issue === 'ignore' || issue === 'i') {
-            await this.toggl.addTag([tracking.id], ['t:no-transmit']);
             transmitting.push({
               tracking, description, redmine, state: 'ignore'
             });
@@ -126,20 +145,24 @@ module.exports = class TransmitCommand extends Command {
       /** @type {T_TransmitItem[]} */
       const merged = [];
       for (const transmit of transmitting) {
+        transmit.when = this.getWhen(transmit.tracking);
+        transmit.customFields = this.getCustomFields(transmit.tracking);
         if (transmit.state !== 'transmit') {
           merged.push(transmit);
           continue;
         }
-        const index = merged.findIndex(v => v.issue === transmit.issue);
+        const index = merged.findIndex(v => v.issue === transmit.issue && v.when.format('DD-MM-YYYY') === transmit.when.format('DD-MM-YYYY') && JSON.stringify(v.customFields || '') === JSON.stringify(transmit.customFields || ''));
         if (index !== -1) {
           if (merged[index].comment === '') merged[index].comment = await this.getComment(transmit);
           const hours = this.getHours(transmit.tracking);
           merged[index].hours += hours;
-          merged[index].merged.push(hours);
+          merged[index].info.push(hours);
+          merged[index].merged.push(transmit.tracking);
         } else {
           transmit.hours = this.getHours(transmit.tracking);
           transmit.comment = await this.getComment(transmit);
-          transmit.merged = [transmit.hours];
+          transmit.info = [transmit.hours];
+          transmit.merged = [transmit.tracking];
           merged.push(transmit);
         }
       }
@@ -160,13 +183,22 @@ module.exports = class TransmitCommand extends Command {
     if (commentMatch && commentMatch[1]) {
       return commentMatch[1].trim();
     } else if (transmit.issue) {
-      return await (transmit.redmine.getIssue(transmit.issue)).subject;
+      return (await (transmit.redmine.getIssue(transmit.issue))).subject;
     } 
     return '';
   }
 
   /**
    * @param {import('../../types').T_TogglTracking} tracking 
+   * @returns {Moment.Moment}
+   */
+  getWhen(tracking) {
+    return Moment.unix(Strtotime(tracking.start));
+  }
+
+  /**
+   * @param {import('../../types').T_TogglTracking} tracking 
+   * @param {boolean} total 
    * @returns {number}
    */
   getHours(tracking, total = false) {
@@ -213,6 +245,14 @@ module.exports = class TransmitCommand extends Command {
    * @param {T_TransmitItem[]} transmitting
    */
   async doTransmit(transmitting) {
+    /** @type {Object<string, T_TransmitItem[]>} */
+    const grouped = {};
+    for (const transmit of transmitting) {
+      const when = Moment.unix(Strtotime(transmit.tracking.start));
+      if (grouped[when.format('DD.MM.YYYY')] === undefined) grouped[when.format('DD.MM.YYYY')] = [];
+      grouped[when.format('DD.MM.YYYY')].push(transmit);
+    }
+
     const header = this.tracker.config.get('commands.transmit.output', {
       description: 'Tracking',
       issue: 'Ticket',
@@ -226,48 +266,89 @@ module.exports = class TransmitCommand extends Command {
     });
 
     const table = new CLITable(header);
-    let totalSkipped = 0;
-    let total = 0;
-    for (const transmit of transmitting) {
-      const comment = transmit.comment ?? await this.getComment(transmit);
-      const hours = this.preprocessHours(transmit.hours ?? this.getHours(transmit.tracking));
-      const activity = this.tracker.config.get('redmine.' + transmit.redmine.project + '.activity', 9);
-      const when = Moment.unix(Strtotime(transmit.tracking.start));
-      const customFields = this.getCustomFields(transmit.tracking);
-
-      if (!this.opts.dry && transmit.state === 'transmit') {
-        await transmit.redmine.createTimeEntry(issue, hours, activity, comment, when, customFields);
-        await this.toggl.addTag([tracking.id], ['t:transmitted']);
+    for (const day in grouped) {
+      table.add([], [this.colorGreen('Date:'), this.colorGreen(day)]);
+      const items = grouped[day];
+      let totalSkipped = 0;
+      let total = 0;
+      for (const transmit of items) {
+        transmit.comment = transmit.comment ?? await this.getComment(transmit);
+        transmit.hours = this.preprocessHours(transmit.hours ?? this.getHours(transmit.tracking));
+        transmit.activity = this.tracker.config.get('redmine.' + transmit.redmine.project + '.activity', 9);
+        transmit.when = transmit.when || this.getWhen(transmit.tracking);
+        transmit.customFields = transmit.customFields ? transmit.customFields : this.getCustomFields(transmit.tracking);
+  
+        if (transmit.state === 'transmit') {
+          total += transmit.hours;
+        } else {
+          totalSkipped += transmit.hours;
+        }
+        const item = {
+          description: transmit.description ? transmit.description : '<no description>',
+          issue: transmit.issue ? transmit.issue : '<no issue>',
+          comment: transmit.comment ? transmit.comment : '<no comment>',
+          activity: transmit.activity, 
+          hours: this.getShowHours(transmit.hours) + ' (' + this.getTime(transmit.hours) + ')',
+          when: transmit.when.format('HH:mm:SS'),
+          project: transmit.redmine.getName(),
+          state: transmit.state,
+        };
+        if (transmit.info) item.info = transmit.info.join(', ');
+        if (transmit.customFields.length) item.info = (item.info ? item.info + ', ' : '') + this.getCustomFieldsInfo(transmit.customFields);
+        table.add(item);
       }
-      if (transmit.state === 'transmit') {
-        total += hours;
-      } else {
-        totalSkipped += hours;
-      }
-      const item = {
-        description: transmit.description ? transmit.description : '<no description>',
-        issue: transmit.issue ? transmit.issue : '<no issue>',
-        comment: comment ? comment : '<no comment>',
-        activity, 
-        hours: this.getShowHours(hours) + ' (' + this.getTime(hours) + ')', 
-        when: when.format('HH:mm:SS DD.MM.YYYY'),
-        project: transmit.redmine.getName(),
-        state: transmit.state,
-      };
-      if (transmit.merged) item.info = transmit.merged.join(', ');
-      table.add(item);
+      table.add(
+        ['Total Skipped:', this.getShowHours(totalSkipped) + ' (' + this.getTime(totalSkipped) + ')'],
+        ['Total:', this.getShowHours(total) + ' (' + this.getTime(total) + ')'],
+      );
     }
-    table.add(
-      {
-        activity: 'Total Skipped:',
-        hours: this.getShowHours(totalSkipped) + ' (' + this.getTime(totalSkipped) + ')',
-      },
-      {
-        activity: 'Total:',
-        hours: this.getShowHours(total) + ' (' + this.getTime(total) + ')',
-      },
-    );
     table.log();
+    if (this.opts.dry) return;
+    if (!this.opts.yes) {
+      console.log();
+      if (await this.tracker.input('Is this ok? (y/n): ') !== 'y') {
+        this.error('Abort');
+        return;
+      }
+    } 
+    
+    const errors = [];
+    for (const day in grouped) {
+      console.log();
+      console.log('Transmit day ' + day + ' ...');
+      for (const transmit of grouped[day]) {
+        if (transmit.state === 'transmit') {
+          process.stdout.write('TRANSMIT: ' + (transmit.issue ? transmit.issue : '<no issue>') + ' - ' + (transmit.comment ? transmit.comment : '<no comment>') + ' - ' + this.getTime(transmit.hours) + ' ... ');
+          try {
+            await transmit.redmine.createTimeEntry(transmit.issue, transmit.hours, transmit.activity, transmit.comment, transmit.when, transmit.customFields);
+            if (transmit.merged) {
+              for (const tracking of transmit.merged) {
+                await this.toggl.addTag([tracking.id], ['t:transmitted']);
+              }
+            } else {
+              await this.toggl.addTag([transmit.tracking.id], ['t:transmitted']);
+            }
+            console.log(this.colorGreen('transmitted'));
+          } catch (error) {
+            this.error('error');
+            errors.push({error, transmit});
+          }
+        } else {
+          console.log(transmit.state.toUpperCase() + ': ' + (transmit.issue ? transmit.issue : '<no issue>') + ' - ' + (transmit.comment ? transmit.comment : '<no comment>') + ' - ' + this.getTime(transmit.hours));
+          if (transmit.state === 'ignore') {
+            await this.toggl.addTag([transmit.tracking.id], ['t:no-transmit']);
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * @param {string} message 
+   * @returns {string}
+   */
+  colorGreen(message) {
+    return '\x1b[32m' + message + '\x1b[0m';
   }
 
   /**
@@ -295,6 +376,30 @@ module.exports = class TransmitCommand extends Command {
       }
     }
     return customFields;
+  }
+
+  /**
+   * @param {array} customFields 
+   * @returns {string}
+   */
+  getCustomFieldsInfo(customFields) {
+    const info = [];
+    for (const field of customFields) {
+      if (field.id === 3) {
+        switch (field.value) {
+          case 1:
+            info.push('Billable');
+            break;
+          case 2:
+            info.push('Non-Billable');
+            break;
+          case 3:
+            info.push('Pauschal');
+            break;
+        }
+      }
+    }
+    return (info ? 'Abbrechnung: ' + info.join(', ') : '');
   }
 
   async initTags(workspace) {
